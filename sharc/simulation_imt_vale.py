UL scheduler fixes - The num_rb_per_ue now is initialized to all ones for proper computation of power control and SINR in the first estimation - Fixed a problem where all remaing RB for the BS was given to all UEs instead on only for this BS link

sharc/simulation_imt_vale.py
@@ -105,9 +105,9 @@ class SimulationImtVale(ABC, Observable):
         self.num_rb_per_bs = np.trunc((1 - self.parameters.imt.guard_band_ratio) *
                                              self.parameters.imt.bandwidth / self.parameters.imt.rb_bandwidth)
 
-        # calculates the number of RB per UE on a given BS
-        #self.num_rb_per_ue = np.trunc(self.num_rb_per_bs/self.parameters.imt.ue_k)
-        self.num_rb_per_ue = np.power(0.1, 50)*np.ones(num_ue)
+        # Number of RB per UE on a given BS. One RB is allocated per connected UE
+        # before the scheduler do the proper allocation
+        self.num_rb_per_ue = np.ones(num_ue)
 
         self.results = Results(self.parameters_filename, self.parameters.general.overwrite_output)
 
@@ -171,10 +171,10 @@ class SimulationImtVale(ABC, Observable):
 
             self.link[bs_index].append(ue_index)
 
-        # UE BW and frequency is the same of it's connected BS.
+        # UE frequency is the same of it's connected BS.
         for bs in self.link.keys():
             for ue in self.link[bs]:
-                self.ue.bandwidth[ue] = self.bs.bandwidth[bs]
+                self.ue.bandwidth[ue] = self.num_rb_per_ue[ue] * self.parameters.imt.rb_bandwidth
                 self.ue.center_freq[ue] = self.bs.center_freq[bs]
 
                 if param.spectral_mask == "ITU 265-E":

sharc/simulation_imt_vale_uplink.py
@@ -131,13 +131,14 @@ class SimulationImtValeUplink(SimulationImtVale):
                         num_available_rbs = num_available_rbs - ue_num_rb
                         allocated_ues.append(ue)
 
-                        # calculating the UE's bandwidth
-                        self.ue.bandwidth[ue] = ue_num_rb * self.parameters.imt.rb_bandwidth
                         # number of RB allocated for the UE
                         self.num_rb_per_ue[ue] = ue_num_rb
                     else:
                         # storing the x and y coordinates of the UE in outage
                         self.get_outage_positions(self.ue.x[ue], self.ue.y[ue])
+
+                if not allocated_ues:
+                    continue
 
                 # self.link only with the allocated UEs
                 self.link[bs] = allocated_ues
@@ -148,10 +149,12 @@ class SimulationImtValeUplink(SimulationImtVale):
 
             # Distribute the remaining RB to the UEs in a round-robin fashion
             n = -1
+            num_rb_per_ue = self.num_rb_per_ue[self.link[bs]]
             while num_available_rbs:
                 n += 1
-                self.num_rb_per_ue[n % len(self.num_rb_per_ue)] += 1
+                num_rb_per_ue[n % len(num_rb_per_ue)] += 1
                 num_available_rbs -= 1
+            self.num_rb_per_ue[self.link[bs]] = num_rb_per_ue
 
             # counting the number of allocated UEs on the given BS
             num_allocated_ues += len(allocated_ues)
@@ -161,6 +164,9 @@ class SimulationImtValeUplink(SimulationImtVale):
 
             # calculating the BS's bandwidth
             self.bs.bandwidth[bs] = self.num_rb_per_bs[bs] * self.parameters.imt.rb_bandwidth
+
+            # calculating the UE's bandwidth
+            self.ue.bandwidth[self.link[bs]] = self.num_rb_per_ue[self.link[bs]] * self.parameters.imt.rb_bandwidth
 
             if total_associated_ues != 0:
                 # calculating the outage for the current drop
@@ -270,7 +276,7 @@ class SimulationImtValeUplink(SimulationImtVale):
             self.bs.rx_power[bs] = self.ue.tx_power[ue]  \
                                         - self.parameters.imt.ue_ohmic_loss \
                                         - self.parameters.imt.ue_body_loss \
-                                        - self.coupling_loss_imt[bs,ue] - self.parameters.imt.bs_ohmic_loss
+                                        - self.coupling_loss_imt[bs, ue] - self.parameters.imt.bs_ohmic_loss
             # create a list of BSs that serve the interfering UEs
             bs_interf = [b for b in bs_active if b not in [bs]]
             # eliminating BSs that don't have associated UEs
@@ -281,7 +287,7 @@ class SimulationImtValeUplink(SimulationImtVale):
                 ui = self.link[bi]
                 interference = self.ue.tx_power[ui] - self.parameters.imt.ue_ohmic_loss  \
                                 - self.parameters.imt.ue_body_loss \
-                                - self.coupling_loss_imt[bs,ui] - self.parameters.imt.bs_ohmic_loss
+                                - self.coupling_loss_imt[bs, ui] - self.parameters.imt.bs_ohmic_loss
 
                 # summing all the interferences
                 interference_linear = np.power(10, 0.1*interference)